5d82e68858b077d111f2aa41a8f7799e85d6d32b
Nicolas Droux <nicolas.droux.2b@gmail.com>
2024-04-15 00:20:29 +0200

    Add mangoose connection and dotenv

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 import express from 'express';
 import router from './routes/appointment-routes.js';
+import mongoose from 'mongoose';
+import { configDotenv } from 'dotenv';
+
+const user = configDotenv().parsed.TB_DB_USER;
+const password = configDotenv().parsed.TB_DB_PASSWORD
 const app = express();
-const port = 3001;
+const port = 3333;
 const version = "v1";
+const uri = `mongodb+srv://${user}:${password}@truckbusters.wivntqs.mongodb.net/?retryWrites=true&w=majority&appName=Truckbusters`;
 
 app.use(`/api/${version}/appointment`, router);
 
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+    try {
+        // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+        await mongoose.connect(uri, clientOptions);
+        await mongoose.connection.db.admin().command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await mongoose.disconnect();
+    }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Test listening on port ${port}`)
 })
+
