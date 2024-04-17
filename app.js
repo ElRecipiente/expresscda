974238d8b4f0b1618336420bf7ff3751c6e814ed
Nicolas Droux <nicolas.droux.2b@gmail.com>
2024-04-17 15:15:40 +0200

    Start using swagger with yml, only default values for now, its a try.

--- app.js
+++ app.js
@@ -2,16 +2,29 @@ import express from 'express';
 import router from './routes/appointment-routes.js';
 import mongoose from 'mongoose';
 import { configDotenv } from 'dotenv';
+import * as swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yaml';
+import fs from 'fs';
 
+// Express config
 const user = configDotenv().parsed.TB_DB_USER;
-const password = configDotenv().parsed.TB_DB_PASSWORD
+const password = configDotenv().parsed.TB_DB_PASSWORD;
 const app = express();
 const port = 3333;
 const version = "v1";
 const uri = `mongodb+srv://${user}:${password}@truckbusters.wivntqs.mongodb.net/?retryWrites=true&w=majority&appName=Truckbusters`;
 
+// Swagger config
+const file  = fs.readFileSync('./swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+// Middleware for API docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Middleware for API routes
 app.use(`/api/${version}/appointment`, router);
 
+// 
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 async function run() {
     try {
@@ -28,5 +41,5 @@ run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Test listening on port ${port}`)
-})
+});
 
